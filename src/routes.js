d76414b3a685c9f46bcf28f4c9df08ea1711a1b5
Gabriel Martins Padoin <contato.gabrielmartinspadoin@gmail.com>
2022-05-21 18:35:52 -0300

    feat: added standard page

--- src/routes.js
+++ src/routes.js
@@ -2,17 +2,18 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
 import Menu from 'components/Menu';
-import Header from 'components/Header';
+import PaginaPadrao from 'components/PaginaPadrao';
 
 export default function AppRouter() {
     return (
         <main>
             <Router>
                 <Menu/>
-                <Header/>
                 <Routes>
-                    <Route path='/' element={<Inicio/>} />
-                    <Route path='/cardapio' element={<Cardapio/>} />
+                    <Route path='/' element={<PaginaPadrao/>}>
+                        <Route index element={<Inicio/>} />
+                        <Route path='cardapio' element={<Cardapio/>} />
+                    </Route>
                 </Routes>
             </Router>
         </main>
